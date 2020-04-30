ccf55c23db0fd44964a9bf45d947fbbda4d80b25
IanChen <ian.chen@taiwantaxi.com.tw>
2020-04-30 17:08:57 +0800

    sub TestService

--- IntegrationTestSample.Test/UnitTest1.cs
+++ IntegrationTestSample.Test/UnitTest1.cs
@@ -3,7 +3,10 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
+using IntegrationTestSample.Controllers;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 using Xunit;
 
 namespace IntegrationTestSample.Test
@@ -18,13 +21,24 @@ namespace IntegrationTestSample.Test
         [Fact]
         public async Task Test1()
         {
-            var httpClient = CreateHttpClient(null);
+            var httpClient = CreateHttpClient(collection =>
+            {
+                collection.AddScoped(r =>
+                {
+                    var testService = Substitute.For<ITestService>();
+                    testService.WeatherForecasts().Returns(new List<WeatherForecast>
+                    {
+                        new WeatherForecast(),
+                    });
+                    return testService;
+                });
+            });
 
             var message = await httpClient.GetAsync("/WeatherForecast");
             var list = await message.Content.ReadAsAsync<List<WeatherForecast>>();
 
             message.StatusCode.Should().Be(HttpStatusCode.OK);
-            list.Count.Should().Be(5);
+            list.Count.Should().Be(1);
         }
     }
 }
